bab8a2d745407725cf8230b68a736c78339acb48
mike-willy <ochiengmikewilliam@gmail.com>
2025-04-08 13:28:22 +0300

    update the code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true, // Enables React Strict Mode for development (helps with identifying potential issues).
+  eslint: {
+    // Automatically runs ESLint on page files during production build.
+    ignoreDuringBuilds: true, // Ignore ESLint errors during the build process (set to false to enforce linting).
+  },
 };
 
 export default nextConfig;
