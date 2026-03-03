f7f9ade3cc889009db0aaf2c58ffb80e3aeaa079
Mike Ochieng <ochiengmikewilliam@gmail.com>
2026-03-03 20:35:00 +0300

    fix: remove unsupported eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Disables linting errors during the build
-    ignoreDuringBuilds: true,
-    // Alternatively, disable specific ESLint rules here
-  },
   reactStrictMode: true,
 };
 
